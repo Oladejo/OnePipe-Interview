f327d10cbc43ea4c5e5bc2f5cd67c22718117c56
Oladejo <bab.oladejo@student.oauife.edu.ng>
2021-01-17 09:46:29 +0100

    add data

---
+++ OnePipe.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnePipe.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
